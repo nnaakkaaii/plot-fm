090d6ab461de2a998fa394ebeaf05dcf2b0ebf32
nnaakkaaii <ultraganbayu10@gmail.com>
2022-08-20 01:47:40 +0900

    エラーハンドリング追加

--- function.py
+++ function.py
@@ -6,7 +6,12 @@ def lambda_handler(event, context):
     >>> lambda_handler({"queryStringParameters": {"name": "ベイカレント"}}, {})
     {}
     """
-    name = event["queryStringParameters"]['name']
+    param = event["queryStringParameters"]
+
+    if 'name' not in param:
+        return {'message': 'パラメータ`name`を指定してください'}
+
+    name = param['name']
 
     c = company.search(name)
     if c is None:
@@ -23,7 +28,7 @@ def lambda_handler(event, context):
             'data': [
                 {
                     'fy': r.fy,
-                    'attr': r.attr,
+                    'attr': r.attr.value,
                     'price': r.price,
                 }
                 for r in p.data
