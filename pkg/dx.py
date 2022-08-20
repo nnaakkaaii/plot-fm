d0fed58a970c19a1db92dec11819f184b31e5c42
nnaakkaaii <ultraganbayu10@gmail.com>
2022-08-20 13:26:31 +0900

    項目増やす

--- pkg/dx.py
+++ pkg/dx.py
@@ -8,7 +8,7 @@ SELL_ADMIN_EXPENSE_CUT_RATE = 0.15
 
 def simulate(p: PL):
     fys = sorted(list(set([x.fy for x in p.data])))
-    header = ['DX前純利益', 'DX後純利益', '販管費削減率']
+    header = ['DX前純利益', 'DX後純利益', 'DX前販管費', 'DX後販管費', '販管費削減率']
 
     pl_dic: Dict[(int, PLAttr), float] = {}
     for row in p.data:
@@ -28,7 +28,11 @@ def simulate(p: PL):
         sell_admin_expense = gross_profit - operating_income
         sell_admin_expense_cut = SELL_ADMIN_EXPENSE_CUT_RATE * sell_admin_expense
         after_dx_net_income = before_dx_net_income + sell_admin_expense_cut
-        data.append([before_dx_net_income, after_dx_net_income, SELL_ADMIN_EXPENSE_CUT_RATE])
+        data.append([before_dx_net_income,
+                     after_dx_net_income,
+                     sell_admin_expense,
+                     sell_admin_expense - sell_admin_expense_cut,
+                     SELL_ADMIN_EXPENSE_CUT_RATE])
     return {
         'data': data,
         'header': header,
